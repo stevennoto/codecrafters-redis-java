35f28ebe2eca0084c813bb7c32efd60b4e7b608c
Steven Noto <snoto@atlassian.com>
2024-09-10 01:39:04 -0500

    Refactored logic.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,41 +7,40 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    //System.out.println("Logs from your program will appear here!");
 
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 6379;
+  private static final int PORT = 6379;
+
+  public static void main(String[] args){
+    ServerSocket serverSocket;
     try {
-      serverSocket = new ServerSocket(port);
+      serverSocket = new ServerSocket(PORT);
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
-      // Send hardcoded PONG for all input for now
-      BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+      // Wait for connection from client and handle
+      handleConnection(serverSocket.accept());
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    }
+  }
+
+  public static void handleConnection(Socket clientSocket) throws IOException {
+    try (clientSocket) {
+      BufferedReader reader = new BufferedReader(
+          new InputStreamReader(clientSocket.getInputStream()));
+      BufferedWriter writer = new BufferedWriter(
+          new OutputStreamWriter(clientSocket.getOutputStream()));
       String line;
       while ((line = reader.readLine()) != null) {
         System.out.println("Received line: " + line);
         if (line.toUpperCase().contains("PING")) {
+          System.out.println("Sending line: PONG");
           writer.write("+PONG\r\n");
           writer.flush();
         }
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
-        }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
     }
   }
 }
